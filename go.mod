b0bc068bdf8e459d5f94bcee55f4a6ddca845965
eric <eric.zsp@hotmail.com>
2021-08-08 17:24:09 +0800

    20210808

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cn-joyconn/modbus
+
+go 1.16
+
+require github.com/cn-joyconn/serial v0.1.0 // indirect
--- test/tcpclient_test.go
+++ test/tcpclient_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/goburrow/modbus"
+	"github.com/cn-joyconn/modbus"
 )
 
 const (
